35095fef0cf8c26ae00c5b797f853fce46fdd744
Walden Lan <446731905@qq.com>
2016-04-30 17:50:02 +0800

    update the homepage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function getyear() {
+    var year=new Date().getFullYear();
+    document.getElementById("current_year").innerHTML=year;
+}
+function getUpdateTime() {
+    var update_time=new Date(document.lastModified);
+    document.getElementById("update_time").innerHTML=update_time;
+}
+$(document).ready(function () {
+    getyear();
+    getUpdateTime();
+});
